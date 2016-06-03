dfabf842f2d7d72b2ffd905abde671a9820e6f15
vinnya3 <vinaceto@yahoo.com>
2016-06-03 19:45:54 -0400

    initial commit

---
+++ js/sudoku.js
@@ -0,0 +1,35 @@
+(function() {
+  var Sudoku = (function(){
+
+    var sudoku_container = document.querySelector('.sudoku');
+
+    var _createSubGrid = function(el){
+      var element = el;
+      for(var i = 0; i < 9; ++i){
+        var box = document.createElement('div'),
+            currentBox = ' box' + i;
+        box.className = 'box' + currentBox;
+        element.appendChild(box);
+      }
+    }
+
+    var addGridModule = function() {
+      //in the game sudoku, there are 9 modules containing 9 sub modules (boxes)
+      for(var i = 0; i < 9; ++i) {
+        //create the grid module, add the class and append to sudoku container
+        var box = document.createElement('div'),
+            currentModule = ' module' + i;
+        box.className = 'sudoku__module' + currentModule;
+        sudoku_container.appendChild(box);
+        //create the sub grid
+        _createSubGrid(box);
+      }
+    }
+    return {
+      createGrid: addGridModule
+    }
+  })(); //end sudoku module
+
+  Sudoku.createGrid();
+
+})();
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+var webpack = require('webpack');
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
+var path = require('path');
+
+module.exports = {
+
+}
