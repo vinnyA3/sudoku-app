719e1c929f4ad0f201213b826bb4ffceee1422d7
vinnya3 <vinaceto@yahoo.com>
2016-06-03 22:41:23 -0400

    added correct entry point and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
 var webpack = require('webpack');
 var precss = require('precss');
 var autoprefixer = require('autoprefixer');
-var path = require('path');
 
 module.exports = {
-
+	entry: './js/main.js',
+	output:{
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	module:{
+		loaders:[
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				loader: 'style-loader!css-loader!postcss-loader'
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				loader: 'style-loader!css-loader!postcss-loader!sass-loader'
+			}
+		]
+	},
+	postcss: function() {
+		return [precss, autoprefixer]
+	},
+	resolve: {
+		extensions: ['','.js']
+	}
 }
