6a03710e193119ea0cf1b738709649d7af435276
vinnya3 <vinaceto@yahoo.com>
2016-06-04 04:21:43 -0400

    added controls and pubsub

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 //require main sass file
 require('../styles/_main.scss');
+//require PubSub
+require('./pubsub.js');
+//require Controls
+require('./controls.js');
 //require sudoku
 require('./sudoku.js');
