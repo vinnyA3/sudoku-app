af519fd42ae693d5f2b112c2afdc4a5f9c0a2dd0
vinnya3 <vinaceto@yahoo.com>
2016-06-05 23:42:01 -0400

    refactored mustache service

--- js/services.js
+++ js/services.js
@@ -1,18 +1,14 @@
 //require jquery
 require('jquery');
 var MustacheService = (function($){
-	var q = new Promise(function(resolve, reject){
-			$.get('./js_templates/box_hovered_template.mst')
-				.success(function(data){
-					resolve(this.data)
-				})
-				.error(function(){
-					var errMsg = 'Could not load template ...';
-					reject(new Error(errMsg));
-				});
-	});
 
-	
+	function _getMustacheTemplate(){
+		return $.get('./js_templates/box_hovered_template.mst')
+			.then(function(data){
+				return data;
+			});
+	}
+
 	return{
 		getTemplate: _getMustacheTemplate
 	}
