63a12dc3e1bfa1532c7d7fc83f0d90f04bd40503
vinnya3 <vinaceto@yahoo.com>
2016-06-08 23:15:38 -0400

    removed closureand refactored require(sudoku) out of a variable initialization

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-(function(){
-	//require main sass file
-	require('../styles/_main.scss');
-	//require Controls
-	require('./controls.js');
-	//require sudoku
-	var Sudoku = require('./sudoku.js');
-	//intialize and create the Sudoku Grid
-	Sudoku.render();
-})();
+//require main sass file
+require('../styles/_main.scss');
+//require Controls
+require('./controls.js');
+//require sudoku
+require('./sudoku.js');
